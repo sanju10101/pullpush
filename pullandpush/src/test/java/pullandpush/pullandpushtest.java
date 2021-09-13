90884ca426a488e7c6a153033422db3d4880caf9
MY-PC <MY-PC@DESKTOP-CB3S4U6>
2021-09-13 20:45:04 +0530

    commited

---
+++ pullandpush/src/test/java/pullandpush/pullandpushtest.java
@@ -0,0 +1,11 @@
+package pullandpush;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class pullandpushtest {
+ @Test
+	public void pullandpush() {
+		Reporter.log("sucessful",true);
+		}
+}
